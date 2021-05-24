43676e2b99b9a4fe8d18f394695ce046bad3387b
lufuhai <2205090563@qq.com>
2021-05-24 23:57:51 +0800

    练习

---
+++ 5_24_19/5_24_19/test.c
@@ -0,0 +1,51 @@
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int k, j, i;
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			k = j * i;
+//			printf("%d=%d*%d\t", k, j, i);
+//		}
+//		printf("\n");
+//	}
+//	//printf("k=j*i %d%d%d", k, j, i);
+//
+//	return 0;
+//}
+
+
+//class Gift {
+//public:
+//	int getValue(vector<int> gifts, int n) 
+//	{
+//		for (int i = 0; i < (gifts.size() / 2 + 1); i++)
+//		{
+//			int k = 0;
+//			for (int j = i; j < gifts.size(); j++)
+//			{
+//				if (gifts[i] == gifts[j])
+//				{
+//					k++;
+//				}
+//			}
+//			if (k > n / 2)
+//			{
+//
+//				return gifts[i];
+//			}
+//		}
+//		return 0;
+//	}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello linux");
+//	return 0;
+//}
