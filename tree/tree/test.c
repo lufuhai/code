ec605af7167c8597941243df700592359c849dba
lufuhai <2205090563@qq.com>
2021-06-05 07:56:28 +0800

    练习

---
+++ tree/tree/test.c
@@ -0,0 +1,85 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 55;
+//	int j;
+//	int k=0;
+//	if (k < 10)
+//	{
+//		for (k = 0; k < 10; k++)
+//		{
+//			printf("请输入一个数：");
+//			scanf("%d", &j);
+//			if (j > i)
+//			{
+//				printf("太大了\n");
+//			}
+//			else if (j < i)
+//			{
+//				printf("太小了\n");
+//			}
+//			else if (j ==55)
+//			{
+//				printf("恭喜你猜对了\n");
+//				break;
+//			}
+//		}
+//		printf("很遗憾你没有猜对\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 55;//给定值得太小
+//	int j;//定义输入的值
+//	int k = 0;
+//	for(k=0;k<10;k++)//设定循环的次数
+//	{
+//		printf("请输入一个数：");
+//		scanf("%d", &j);  //输入一个数
+//		if (j > i)
+//		{
+//			printf("太大了\n");
+//		}
+//		else if (j < i)
+//		{
+//			printf("太小了\n");
+//		}
+//		else if(j=55)
+//		{
+//			printf("恭喜你猜对了\n");
+//			break;  //结束循环
+//		}
+//	} 
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	//printf(“input three data : \n”);//使用 scanf 之前先用 printf 提示输入。
+//	scanf("%d%d%d", &a, &b, &c); // scanf 的“输入参数”中，前面的取地址符&记住
+//	printf("%d,%d,%d\n", a, b, c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("请输入一个数:");
+//	scanf("%d", &a);
+//	printf("%d", a);
+//	return 0;
+//}
+//
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello");
+//	return 0;
+//}
