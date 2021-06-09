a10e2c680c8a1f3866ee0383ee3002521222fb26
lufuhai <2205090563@qq.com>
2021-06-09 22:55:37 +0800

    练习

---
+++ 5_31/5_31/test.c
@@ -0,0 +1,57 @@
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i=10;
+//	int num = 55;
+//	    /*printf("请输入一个数：%d",i);
+//		scanf("%d", &i);*/
+//		if (i < num)
+//		{
+//			printf("太小了");
+//		}
+//		else if (i > num)
+//		{
+//			printf("太大了");
+//		}
+//		else
+//		{
+//			printf("恭喜你猜对了");
+//		}	
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i, j, k;
+//	for (i = 1; i <=5; i++)
+//	{
+//		for (j = 1; j < 5 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (k = 1; k <2*i-1; k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int count;
+//	for (count = 1; count < 10; count++)
+//	{
+//		srand(count + 1);
+//		printf("Random number %d is : %d\n", count, rand());
+//	}
+//	return 0;
+//}
